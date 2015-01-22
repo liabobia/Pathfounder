f94084881434b83fa89b66bffa16e97dcd5c5b7a
liabobia <liabobia@users.noreply.github.com>
2015-01-22 14:59:06 -0500

    Update barbarian.rb

--- app/pf_classes/barbarian.rb
+++ app/pf_classes/barbarian.rb
@@ -1,1 +1,23 @@
-#this is the thing
+class Barbarian < PfClass
+
+  role :class
+  hit_die 12
+  alignment :chaotic
+
+  skills :acrobatics, :craft, :climb, :intimidate, :handle_animal, :knowledge_nature, :perception, :ride, :survival, :swim
+  skill_ranks_per_level 4
+  skill_ranks_per_level int modifier 
+
+  base_attack_bonus      1
+  mods feat_count:       1 
+  mods fortitude_save:  -2
+  mods will_save:        0
+  mods reflex_save:      0
+  mods speed             +10
+  mods rage              4
+  mods rage_bonus        con modifier
+
+  adds weapon_proficiency: [:simple, :martial]
+  adds armor_proficiency: [:light, :medium]
+  adds shield_proficiency: [:light_wooden, :light_steel, :heavy_wooden, :heavy_steel, :buckler]
+end
